7b08f93b3f81324e9a59499e74a5ccb160bea8aa
Qurclinc <pastghost2@mail.ru>
2025-03-27 15:13:18 +0500

    Bash/lab3/task1 fixed

---
+++ Bash/lab3/task1/task1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "You must defer user!"
+    exit 1
+fi
+
+USER="$1"
+ps aux | grep "$USER" | awk '{print $2 ":" $11}' | head -n-4 > processes.txt
+cat processes.txt | wc -l
