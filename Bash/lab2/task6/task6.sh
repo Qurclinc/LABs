581107e57dfd907bb11dd116e7852b0d39d97484
Qurclinc <pastghost2@mail.ru>
2025-04-04 09:08:15 +0500

    I missed quote marks XD

--- Bash/lab2/task6/task6.sh
+++ Bash/lab2/task6/task6.sh
@@ -4,7 +4,7 @@ exec 2>/dev/null
 
 k=0
 FILEPATH="/var/log"
-for file in $( find "$FILEPATH" -name *.log -type f 2>/dev/null ); do
+for file in $( find "$FILEPATH" -name "*.log" -type f 2>/dev/null ); do
     (( k+=$( wc -l "$file" 2>/dev/null | awk '{print $1}') ))
 done
 
