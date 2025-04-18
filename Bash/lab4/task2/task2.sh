74388948609253957ca3d9519af9d793ba86ff69
adam <adam@astra>
2025-04-18 10:08:54 +0500

    Bash lab4 task2 done

---
+++ Bash/lab4/task2/task2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SOURCE_LOCATION='../task1'
+SCRIPT_NAME="task1.sh"
+# SCRIPT="$SOURCE_LOCATION/$SCRIPT_NAME"
+REPORT="$SOURCE_LOCATION/report"
+echo "cd $SOURCE_LOCATION && ./$SCRIPT_NAME" | at now + 2 minute
+
+tail -f $REPORT
