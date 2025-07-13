fe1003c5076b62fb00315e36d207bc19554d3109
nkreali <138723899+nkreali@users.noreply.github.com>
2025-07-13 16:58:39 +0500

    Update task2.sh
    cleaned code

--- Bash/lab4/task2/task2.sh
+++ Bash/lab4/task2/task2.sh
@@ -2,7 +2,6 @@
 
 SOURCE_LOCATION='../task1'
 SCRIPT_NAME="task1.sh"
-# SCRIPT="$SOURCE_LOCATION/$SCRIPT_NAME"
 REPORT="$SOURCE_LOCATION/report"
 echo "cd $SOURCE_LOCATION && ./$SCRIPT_NAME" | at now + 2 minute
 
