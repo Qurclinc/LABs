f25556b0de1e99e30e56bb18b6b9be0415faa4de
Qurclinc <pastghost2@mail.ru>
2025-10-11 18:41:56 +0500

    update

--- Numerical Analysis/lab1/iterations_method.py
+++ Numerical Analysis/lab1/iterations_method.py
@@ -1,14 +1,18 @@
 from math import sqrt, sin, cos
 
-# def phi(x: float) -> float:
-#     return 1 / sqrt(x + 1)
+def phi(x: float) -> float:
+    return 1 / sqrt(x + 1)
 
 def phi(x: float) -> float:
-    U = 0.0769
-    return x - U * (x**2 - 10 * sin(x))
+    U = 0.9843
+    return x - U * (sqrt(x + 1) - (1 / x))
+
+# def phi(x: float) -> float:
+#     U = 0.0769
+#     return x - U * (x**2 - 10 * sin(x))
 
 def print_info(n: int, xn1: float, xn: float):
-    print(f"{n:<5}\t{xn1:<12.8f}\t{xn:<12.8f}\t{abs(xn1-xn):<12.8f}")
+    print(f"{n:<5}\t{xn1:<12.8f}\t{xn:<12.8f}\t{abs(xn1-xn):<12.8f} ")
 
 step = 0
 def iterations_method(x: float, E: float = 0.000001) -> float:
@@ -21,5 +25,5 @@ def iterations_method(x: float, E: float = 0.000001) -> float:
     return iterations_method(xn)
 
 if __name__ == "__main__":
-    x0 = 2.45
+    x0 = 0.75
     iterations_method(x0)
