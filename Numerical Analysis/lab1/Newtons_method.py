3f670d24bb7dc58a149d68f216d3b21770ba5cf9
Qurclinc <pastghost2@mail.ru>
2025-10-12 22:45:52 +0500

    upd

--- Numerical Analysis/lab1/Newtons_method.py
+++ Numerical Analysis/lab1/Newtons_method.py
@@ -9,7 +9,7 @@ def df(x: float) -> float:
     return (1 / (2 * sqrt(x + 1))) + (1 / x ** 2)
 
 def print_info(n: int, xn: float, xi: float):
-    print(f"{step:<5}\t{xi:<12.8f}\t{f(xi):<12.12f}\t{df(xi):<12.8f}\t{abs(xn-xi):<12.12f}")
+    print(f"{step - 1:<5}\t{xi:<12.8f}\t{f(xi):<12.12f}\t{df(xi):<12.8f}\t{abs(xn-xi):<12.12f}")
 
 step = 0
 def Newtons_method(xi: float, E: float = 0.000001) -> float:
--- Numerical Analysis/lab1/Newtons_method2.py
+++ Numerical Analysis/lab1/Newtons_method2.py
@@ -9,15 +9,15 @@ def df(x: float) -> float:
     return (1 / (2 * sqrt(x + 1))) + (1 / x ** 2)
 
 def print_info(n: int, x_new: float, q: float):
-    print(f"{n:<5}\t{x_new:<12.8f}\t{f(x_new):<12.8f}\t{df(x_new):<12.8f}\t{q:<12.8f}")
+    print(f"{n:<5}\t{x_new:<12.8f}\t{f(x_new):<12.8f}\t{df(x_new):<12.8f}\t{q:<12.12f}")
 
 step = 0
 def Newtons_method(xi: float, E: float = 0.000001) -> float:
     global step
     step += 1
     xn = xi - (f(xi) / df(xi))
-    q = abs(f(xn)) / 1.9352  # ДОБАВЛЕН МОДУЛЬ!
-    print_info(step - 1, xn, q)
+    q = abs(f(xi)) / 1.9352  # ДОБАВЛЕН МОДУЛЬ!
+    print_info(step - 1, xi, q)
     if q < E:
         return xn
     return Newtons_method(xn)
